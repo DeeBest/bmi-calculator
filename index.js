66d023ec81df622746a95c4e18f8d15a3c844085
DeeBest <simphiwedladla8@gmail.com>
2024-05-02 12:49:00 +0200

    created function to calculate bmi and display it

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const currentYear = new Date().getFullYear();
 const footerParagraph = document.getElementById('footer-paragraph');
 footerParagraph.textContent = `Created By \xA9Simphiwe ${currentYear}.`;
 
-const heightInput = document.getElementById('height-input');
-const weightInput = document.getElementById('weight-input');
 const calculateBmiBtn = document.getElementById('calculate-bmi-btn');
 const resultsOutput = document.getElementById('results-output');
 const weightConditionOutput = document.getElementById(
   'weight-condition-output'
 );
+
+calculateBmiBtn.addEventListener('click', calculateBmiFunction);
+
+function calculateBmiFunction() {
+  const heightInputValue = +document.getElementById('height-input').value;
+  const weightInputValue = +document.getElementById('weight-input').value;
+
+  const heightInMeters = heightInputValue / 100;
+
+  const bmiValue = weightInputValue / (heightInMeters * heightInMeters);
+
+  resultsOutput.value = `Your BMI is ${bmiValue.toFixed(2)}.`;
+}
