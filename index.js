5a58ac3b178aa381ddceba53f98dd7de10b18899
DeeBest <simphiwedladla8@gmail.com>
2024-05-02 12:02:01 +0200

    imported the html elements to  javascript

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const currentYear = new Date().getFullYear();
 
 const footerParagraph = document.getElementById('footer-paragraph');
 footerParagraph.textContent = `Created By \xA9Simphiwe ${currentYear}.`;
+
+const heightInput = document.getElementById('height-input');
+const weightInput = document.getElementById('weight-input');
+const calculateBmiBtn = document.getElementById('calculate-bmi-btn');
+const resultsOutput = document.getElementById('results-output');
+const weightConditionOutput = document.getElementById(
+  'weight-condition-output'
+);
