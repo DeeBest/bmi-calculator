5bdcef19c936a8d00a8e205d0c61a36d0843b37a
DeeBest <simphiwedladla8@gmail.com>
2024-05-02 13:39:15 +0200

    added if statement for verifying if the user put correct details

--- index.js
+++ index.js
@@ -15,19 +15,26 @@ function calculateBmiFunction() {
   const heightInputValue = +document.getElementById('height-input').value;
   const weightInputValue = +document.getElementById('weight-input').value;
 
-  const heightInMeters = heightInputValue / 100;
+  if (
+    heightInputValue > 0 &&
+    heightInputValue < 1000 &&
+    weightInputValue > 0 &&
+    weightInputValue < 3000
+  ) {
+    const heightInMeters = heightInputValue / 100;
 
-  const bmiValue = weightInputValue / (heightInMeters * heightInMeters);
+    const bmiValue = weightInputValue / (heightInMeters * heightInMeters);
 
-  resultsOutput.value = `Your BMI is ${bmiValue.toFixed(2)}.`;
+    resultsOutput.value = `Your BMI is ${bmiValue.toFixed(2)}.`;
 
-  if (bmiValue < 18.5) {
-    weightConditionOutput.value = 'Weight Condition: Under Weight';
-  } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
-    weightConditionOutput.value = 'Weight Condition: Normal Weight';
-  } else if (bmiValue >= 25 && bmiValue <= 29.9) {
-    weightConditionOutput.value = 'Weight Condition: Over Weight';
-  } else {
-    weightConditionOutput.value = 'Weight Condition: Obesity';
-  }
+    if (bmiValue < 18.5) {
+      weightConditionOutput.value = 'Weight Condition: Under Weight';
+    } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
+      weightConditionOutput.value = 'Weight Condition: Normal Weight';
+    } else if (bmiValue >= 25 && bmiValue <= 29.9) {
+      weightConditionOutput.value = 'Weight Condition: Over Weight';
+    } else {
+      weightConditionOutput.value = 'Weight Condition: Obesity';
+    }
+  } else alert('Please put correct details.');
 }
